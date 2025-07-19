99673e5ee32ba4c61bd73d7d86c624ff461481e4
AwsSupOps <100229797+AwsSupOps@users.noreply.github.com>
2025-07-19 09:10:56 +0530

    Update page.tsx
    using server ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-"use client";
+/*
+  "use client";
 
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
@@ -50,3 +51,47 @@ export default function App() {
     </main>
   );
 }
+
+*/
+
+// app/page.tsx
+
+import { revalidatePath } from "next/cache";
+
+import { AuthGetCurrentUserServer, cookiesClient } from "@/utils/amplify-utils";
+
+//import Logout from "@/components/Logout";
+
+async function App() {
+  const user = await AuthGetCurrentUserServer();
+  const { data: todos } = await cookiesClient.models.Todo.list();
+
+  async function addTodo(data: FormData) {
+    "use server";
+    const title = data.get("title") as string;
+    await cookiesClient.models.Todo.create({
+      content: title,
+      done: false,
+      priority: "medium",
+    });
+    revalidatePath("/");
+  }
+
+  return (
+    <>
+      <h1>Hello, Amplify 👋</h1>
+      
+      <!-- {user && <Logout />}  -->
+      <form action={addTodo}>
+        <input type="text" name="title" />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <ul>
+        {todos && todos.map((todo) => <li key={todo.id}>{todo.content}</li>)}
+      </ul>
+    </>
+  );
+}
+
+export default App;
